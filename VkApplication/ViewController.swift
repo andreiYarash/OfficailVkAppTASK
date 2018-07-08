3538fde269550c1bc57b918e25560e2f3551c2e3
Andrei Yarash <andreiyarash@Andreis-MacBook-Pro.local>
2018-07-09 02:47:17 +0300

    add new file VKlogic,change some methods

--- VkApplication/ViewController.swift
+++ VkApplication/ViewController.swift
@@ -1,37 +1,35 @@
 import Foundation
 import VK_ios_sdk
 class ViewController: UIViewController,VKSdkDelegate,VKSdkUIDelegate{
+    @IBOutlet weak var userName: UILabel!
     
     fileprivate let vk_Token:String = "ca7f26a2dd5fcc439548accfe8cd76ce13a0fcbefc8b5a684850586f10e32b2d843ea5f94413ecbcbd001"
     
     let instanceVK = VKSdk.initialize(withAppId: "6627138")
+    
+    var instanceParsingData:VkApiMethodGetMethod = VkApiMethodGetMethod()
+
     func vkSdkShouldPresent(_ controller: UIViewController!) {
         
        self.present(controller!, animated: true, completion: nil)
         
-                        //Run Safari
-        
-//                if (self.presentedViewController != nil){
-//                    self.dismiss(animated: true) {
-//                        self.present(controller!, animated: true, completion: {
-//                            print("run Safari")
-//                        })
-//                    }
-//                }else{
-//                    self.present(controller, animated: true) {
-//                        print("open safari!")
-//                    }
-//                }
     }
     
     func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
         print(captchaError.errorText!)
     }
+
     
     func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
-     
-        guard let tokinKeyIn = result.token.accessToken else{return}
-        print(tokinKeyIn)
+        print(result.token.expiresIn)
+        if result.token.userId != nil {
+            
+        self.instanceParsingData.tokenKey = result.token.accessToken
+            let dataDecoding = instanceParsingData.requestData()
+        self.userName.text = dataDecoding?.response.last_name
+            print(dataDecoding?.response.first_name)
+        }
+        
     
     }
 
@@ -62,17 +60,16 @@ class ViewController: UIViewController,VKSdkDelegate,VKSdkUIDelegate{
         
     }
     
+
    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        prepareVkServices()
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         //Delegates
         instanceVK?.register(self)
         instanceVK?.uiDelegate = self
+        prepareVkServices()
     }
 }
 
--- VkApplication/VkLogic.swift
+++ VkApplication/VkLogic.swift
@@ -1,1 +1,53 @@
 import Foundation
+import VK_ios_sdk
+
+struct LogicVkService:Decodable{
+    struct ArrayObject:Decodable {
+        let id:Int?
+        let first_name:String?
+        let last_name:String?
+    }
+    let response:ArrayObject
+}
+
+struct VkApiMethodGetMethod{
+    var tokenKey:String?
+    var result:LogicVkService?
+   
+    
+    mutating func requestData() ->LogicVkService?{
+         var urlString:String = "https://api.vk.com/method/users.get?&access_token="
+   
+        guard let truetokenKey = tokenKey else{return nil}
+      urlString += truetokenKey
+        urlString += "&v=5.80"
+   
+        print(urlString)
+        
+        guard let urlRequest = URL(string: urlString) else{return nil}
+        
+        
+        var resultMethod:LogicVkService?
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, _, err) in
+            DispatchQueue.main.async {
+                guard let data = data else{return}
+                
+                let jsconDecoder = JSONDecoder()
+                
+                do {
+                  
+                    resultMethod = try? jsconDecoder.decode(LogicVkService.self, from: data)
+                    
+                    
+
+                }catch let errorParse{
+                    print("parse error:\(errorParse)")
+                }
+                
+            }
+        }.resume()
+     return resultMethod
+    }
+    
+}
