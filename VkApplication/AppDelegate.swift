065f76bdc9a6182b31ee834f19352ca78e2250ae
Andrei Yarosh <andreiyarash@andreis-macbook-pro.local>
2018-07-08 20:18:25 +0300

    add module window, add vk app authorization

--- VkApplication/AppDelegate.swift
+++ VkApplication/AppDelegate.swift
@@ -12,14 +12,20 @@ import VK_ios_sdk
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+ 
+    
+   
 
     var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-       
+
         return true
     }
+   
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -73,6 +79,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         })
         return container
     }()
+    // MARK: - Vk Delegate Method
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        VKSdk.processOpen(url, fromApplication: options[.sourceApplication] as! String)
+        return true
+    }
+ 
+    
+    
+    
+
+    
 
     // MARK: - Core Data Saving support
 
--- VkApplication/ViewController.swift
+++ VkApplication/ViewController.swift
@@ -1,25 +1,82 @@
+import Foundation
+import VK_ios_sdk
+class ViewController: UIViewController,VKSdkDelegate,VKSdkUIDelegate{
+    fileprivate let vk_Token:String = "ca7f26a2dd5fcc439548accfe8cd76ce13a0fcbefc8b5a684850586f10e32b2d843ea5f94413ecbcbd001"
+    
+    let instanceVK = VKSdk.initialize(withAppId: "6627138")
+    func vkSdkShouldPresent(_ controller: UIViewController!) {
+        
+       self.present(controller!, animated: true, completion: nil)
+        
+        //Run safari!!!
+//                if (self.presentedViewController != nil){
+//                    self.dismiss(animated: true) {
+//                        self.present(controller!, animated: true, completion: {
+//                            print("run Safari")
+//                        })
+//                    }
+//                }else{
+//                    self.present(controller, animated: true) {
+//                        print("open safari!")
+//                    }
+//                }
 //
-//  ViewController.swift
-//  VkApplication
-//
-//  Created by Andrei Yarash on 7/7/18.
-//  Copyright © 2018 Andrei Yarash. All rights reserved.
-//
+    }
+    
+    func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
+        print(captchaError.errorText!)
+    }
+    
+    func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
+     
+        guard (result.token.accessToken) != nil else{
+            print("ddfdf")
+            return
+        }
+     print(result.token.accessToken!)
+    
+    }
 
-import UIKit
-
-class ViewController: UIViewController {
+    
+    func vkSdkUserAuthorizationFailed() {
+        print("erroro authorized!")
+    }
+    
+   
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let scopePermissions = ["email", "friends", "wall", "offline", "photos", "notes"]
+        VKSdk.wakeUpSession(["email","offline","friends"]) { (state, err) in
+            if state == VKAuthorizationState.authorized{
+                VKSdk.forceLogout()
+                print("yes go")
+            }else{
+                print("No,error:")
+            }
+            
+            if VKSdk.vkAppMayExists() == true{
+                VKSdk.authorize(scopePermissions, with: .unlimitedToken)
+                
+            }else{
+                VKSdk.authorize(scopePermissions, with:[.disableSafariController,.unlimitedToken])
+            }
+        }
+        
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        instanceVK?.register(self)
+        instanceVK?.uiDelegate = self
+        
+      
+        
+ 
+      
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
